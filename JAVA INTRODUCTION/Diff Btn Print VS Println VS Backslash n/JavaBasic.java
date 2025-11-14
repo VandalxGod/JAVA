bb1a433e9820128362daa0f922558bd076bd5b8c
VandalxGod <sumit.tutu2506@gmail.com>
2025-11-14 02:46:45 -0800

    first commit

---
+++ JAVA INTRODUCTION/Diff Btn Print VS Println VS Backslash n/JavaBasic.java
@@ -0,0 +1,19 @@
+
+
+public class JavaBasic {
+    public static void main(String[] args){
+
+        // Using print
+        System.out.print("Hello");
+        System.out.print(" World");
+
+        // Using println
+        System.out.println("Hello");
+        System.out.println("World");
+
+        // Using newline character \n
+        System.out.print("Hello\n");
+        System.out.print("World");
+    }
+    
+}
